dca9d613fa17ba74abe1a1027eb596798959d7e7
Sander Krul <sander@dope-e.nl>
2026-09-21 14:26:11 +0200

    Handle concurrent user registration

---
+++ src/Application/Authentication/Exception/DuplicateUserEmail.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CurlySanders\JobApplicationTracker\Application\Authentication\Exception;
+
+final class DuplicateUserEmail extends \RuntimeException
+{
+}
